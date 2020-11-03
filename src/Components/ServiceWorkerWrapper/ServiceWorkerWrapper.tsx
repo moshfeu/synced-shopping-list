21313b42d782291fe0f9a4d0082772d7cdf7e742
Mosh Feu <moshfeu@gmail.com>
2020-11-04 00:40:20 +0200

    fix(sw): combine regular & cache

--- src/Components/ServiceWorkerWrapper/ServiceWorkerWrapper.tsx
+++ src/Components/ServiceWorkerWrapper/ServiceWorkerWrapper.tsx
@@ -15,11 +15,6 @@ export const ServiceWorkerWrapper: FC = () => {
 
   useEffect(() => {
     serviceWorker.register({ onUpdate: onSWUpdate });
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw-cache.js')
-        .then(() => 'sw-cache registered');
-    }
   }, []);
 
   const reloadPage = () => {
