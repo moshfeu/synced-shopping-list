40da200cf82edd1008064b8e5533b9eb451d12f8
Mosh Feu <moshfeu@gmail.com>
2020-11-03 09:33:06 +0200

    chore: don't bump version on branch push

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-yarn build
-if [[ $NO_VERSION ]]; then
-  echo "ok, I won't create a version"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "master" ]]; then
+  echo "⏭️ NM. I'll just push";
 else
-  npm version minor
+  yarn build
+  if [[ $NO_VERSION ]]; then
+    echo "ok, I won't create a version"
+  else
+    npm version minor
+  fi
 fi
