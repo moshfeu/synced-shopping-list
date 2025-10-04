9aa7c9f87ec2d5d0478e8e89617130f20cfd95e6
Mosh Feu <moshfeu@gmail.com>
2025-10-04 10:14:25 +0300

    feat: require quantity after adding from search

--- src/Components/List/ItemDetails.tsx
+++ src/Components/List/ItemDetails.tsx
@@ -12,6 +12,7 @@ import {
   CardMedia,
   CircularProgress,
   FormControl,
+  FormControlLabel,
   FormLabel,
   Grid,
   IconButton,
@@ -22,7 +23,11 @@ import {
   TextField,
   ToggleButtonGroupProps,
 } from '@mui/material';
-import { ToggleButtonGroup, ToggleButton } from '@mui/material';
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Switch as MuiSwitch,
+} from '@mui/material';
 import ImagePlaceholder from '../../Assets/imagePlaceholder.svg';
 import { useDB } from '../../Hooks/useDB';
 import { useDeleteListItem } from '../../Hooks/useDeleteListItem';
@@ -73,10 +78,14 @@ export const ItemDetails: FC<ItemDetailsProps> = ({ listItem }) => {
   ) {
     let value = null;
     let name = null;
+    let type = null;
 
-    ({ value, name } = (e.currentTarget as HTMLInputElement) || {});
+    ({ value, name, type } = (e.currentTarget as HTMLInputElement) || {});
     if (!name) {
-      ({ value, name } = e.target as HTMLInputElement);
+      ({ value, name, type } = e.target as HTMLInputElement);
+    }
+    if (type === 'checkbox') {
+      value = (e.target as HTMLInputElement).checked;
     }
     if (name?.startsWith('item_')) {
       const [, prop] = name.split('_');
@@ -323,6 +332,18 @@ export const ItemDetails: FC<ItemDetailsProps> = ({ listItem }) => {
                     variant='standard'
                   />
                 </FormControl>
+                <FormControl classes={{ root: classes.formControl }}>
+                  <FormControlLabel
+                    control={
+                      <MuiSwitch
+                        checked={!!listItem.item.requiredQuantity}
+                        name='item_requiredQuantity'
+                        onChange={onChange}
+                      />
+                    }
+                    label='Required Quantity'
+                  />
+                </FormControl>
                 {listItem.addedBy && (
                   <FormControl classes={{ root: classes.formControl }}>
                     <Tooltip title={getTooltipContent(listItem)}>
@@ -352,8 +373,6 @@ export const ItemDetails: FC<ItemDetailsProps> = ({ listItem }) => {
                 classes={{ root: classes.modal }}
                 open={open}
                 onClose={handleClose}
-                aria-labelledby='parent-modal-title'
-                aria-describedby='parent-modal-description'
               >
                 {/* https://stackoverflow.com/a/73130768/863110 */}
                 <>
--- src/Components/List/Search/Search.tsx
+++ src/Components/List/Search/Search.tsx
@@ -1,17 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Search as SearchIcon,
   History as HistoryIcon,
 } from '@mui/icons-material';
-import { IconButton, Divider } from '@mui/material';
+import { IconButton, Divider, TextField } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { useAuth } from '../../../Hooks/useAuth';
 import { useDB } from '../../../Hooks/useDB';
 import { useUIStore } from '../../../Hooks/useUIStore';
 import { addListItem, updateListItem } from '../../../Services/db';
 import { Item, ListItemView } from '../../../Types/entities';
+import type { ListItem } from '../../../Types/entities';
 import { Header } from '../../Header/Header';
+import { FormDialog } from '../../Dialogs/FormDialog/FormDialog';
 
 type Option = string | Item;
 
@@ -30,6 +32,7 @@ export const Search: FC = () => {
   const { items, list } = useDB();
   const { dispatch } = useUIStore();
   const { currentUser } = useAuth();
+  const [pendingItem, setPendingItem] = useState<ListItem | null>(null);
 
   function getItemOrNew(option: Option): Parameters<typeof addListItem>[0] {
     if (typeof option === 'string') {
@@ -65,7 +68,7 @@ export const Search: FC = () => {
     }
   }
 
-  function onAdd(option: Option) {
+  async function onAdd(option: Option) {
     if (typeof option === 'string') {
       const itemInList = findItemInList(option);
       if (itemInList) {
@@ -74,10 +77,28 @@ export const Search: FC = () => {
       }
     }
     const item = getItemOrNew(option);
-    addListItem(item, currentUser);
+    const listItem = await addListItem(item, currentUser);
+    setPendingItem(listItem);
+  }
+
+  function handleClose() {
+    setPendingItem(null);
+  }
+
+  function handleUpdateQuantity(formData: FormData) {
+    if (pendingItem) {
+      const quantity = Number(formData.get('quantity'));
+      if (quantity && !isNaN(quantity) && quantity > 0) {
+        updateListItem(pendingItem, {
+          quantity,
+        });
+        handleClose();
+      }
+    }
   }
 
   return (
+    <>
     <Header
       onSubmit={onAdd}
       input={{ placeholder: 'Product Name', options: items }}
@@ -94,5 +115,32 @@ export const Search: FC = () => {
         <HistoryIcon />
       </IconButton>
     </Header>
+    {
+      pendingItem && (
+        <FormDialog
+          title="Quantity"
+          content={
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="quantity"
+              label="Quantity"
+              type="number"
+              fullWidth
+              variant="standard"
+              InputProps={{
+                inputProps: { min: 1 },
+              }}
+            />
+          }
+          actionText="Update"
+          closeDialog={handleClose}
+          onAction={handleUpdateQuantity}
+        />
+      )
+    }
+    </>
   );
 };
